f83c9b1f05a0dd79723b744e8634ee371193f67a
Cheebachong <atrodondi@gmail.com>
2020-06-04 18:37:10 -0700

    Updates

---
+++ precache-manifest.8dba0fda79ff8f67f60018047af00e2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4ac79249c175d5a5183e1411698862",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "f1033ce4a6167d170c72",
+    "url": "/Clicky-Game/static/css/main.eaeefe8e.chunk.css"
+  },
+  {
+    "revision": "2bd0f7eaf8daae6a35fc",
+    "url": "/Clicky-Game/static/js/2.c581d9da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.c581d9da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1033ce4a6167d170c72",
+    "url": "/Clicky-Game/static/js/main.e3578fae.chunk.js"
+  },
+  {
+    "revision": "623390ca189948324e2d",
+    "url": "/Clicky-Game/static/js/runtime-main.525981fe.js"
+  }
+]);
